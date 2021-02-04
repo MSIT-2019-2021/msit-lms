45ac46ccddc7ab4b25417dba08e40036ec85714a
Sravan-m <65647304+Sravan-m@users.noreply.github.com>
2021-02-04 16:38:29 +0530

    Added new functionality

--- src/Routes.js
+++ src/Routes.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './Pages/Home';
-import test from './Pages/t'
 
-const Routes = () => {
+const route = () => {
   return (
     <Switch> {/* The Switch decides which component to show based on the current URL.*/}
       <Route exact path='/' component={Home}></Route>
-      <Route exact path='/test' component={test}></Route>
     </Switch>
   );
 }
 
-export default Routes;
+export default route;
